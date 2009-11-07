9183a95b26367c252390e1c00ce3f6465c9166b6
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2009-11-07 21:26:53 +0200

    add configuration properties

--- init.rb
+++ init.rb
@@ -1,7 +1,9 @@
-require 'rest_facebook/acts_with_fb_connect'
-require 'rest_facebook/connect'
+require 'rest_facebook'
 require 'rest_facebook/connect_helper'
 require 'rest_facebook/session'
+require 'rest_facebook/acts_with_fb_connect'
+
+RestFacebook.load_app_config( "#{RAILS_ROOT}/config/rest_facebook.yml")
 
 ActionController::Base.send :include, RestFacebook::ActsWithFbConnect
 ActionView::Base.send :include, RestFacebook::ConnectHelper
--- lib/rest_facebook.rb
+++ lib/rest_facebook.rb
@@ -1,16 +1,30 @@
-module RestFacebook::Connect
-  
+module RestFacebook
+
   API_VERSION               = "1.0"
 
   API_HOST                  = "api.facebook.com"
   API_PATH_REST             = "/restserver.php"
-
-  APP_API_KEY               = "4e4354693b7a6305d3e8bb616d8ebfe4"
-  APP_SECRET_KEY            = "c433722b5d4659e954a0e1d340819fac"
   
+  
+  def self.yml
+    @facebook_apps
+  end
+
+  def self.load_app_config yaml_file
+    raise StandardError.new ("Can't find configuration file") unless File.exists? yaml_file
+    
+    yaml = YAML.load( ERB.new( File.read( yaml_file)).result)
+    yaml = yaml[RAILS_ENV] if defined? RAILS_ENV
+    
+    raise StandardError.new("Can't find required properties") unless check_app_config yaml
+    
+    RAILS_DEFAULT_LOGGER.info("REST Facebook is configured")
+    
+    @facebook_apps = yaml
+  end
   
   def self.new_fb_session
-    RestFacebook::Session.new( APP_API_KEY, APP_SECRET_KEY)
+    RestFacebook::Session.new( @api_key, @secret_key)
   end
   
   def self.get_auth_fb_session( auth, ss=false)
@@ -23,7 +37,7 @@ module RestFacebook::Connect
   
   def self.load_fb_session_from( cookies)
     parsed = {}
-    fb_cookie_prefix = APP_API_KEY+'_'
+    fb_cookie_prefix = @api_key+'_'
     fb_cookie_names = cookies.keys.select{ |k| k && k.starts_with?( fb_cookie_prefix)}
     fb_cookie_names.each { |key| parsed[ key[ fb_cookie_prefix.size,key.size]] = cookies[ key] }
     
@@ -38,5 +52,11 @@ module RestFacebook::Connect
     s.init_session_state parsed
     s
   end
+  
+  private
+  
+    def self.check_app_config yaml
+      (@api_key = yaml[ 'api_key'] if yaml[ 'api_key']) and (@secret_key = yaml[ 'secret_key'] if yaml[ 'secret_key'])
+    end
   
 end
